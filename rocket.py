f2b3784c4bdf67ab959b26743575d82d66f95a55
Denis Mondok <denis.mondok.2019.1@gmail.com>
2026-06-29 19:54:47 +0200

    rocket1

--- rocket.py
+++ rocket.py
@@ -1,4 +1,8 @@
+import time
+
 countdawn = 5
 while countdawn > 0:
     print ("до старту залишилося " + str (countdawn) + "...")
     countdawn = countdawn -1 # зменшуємо число
+    time.sleep(1)
+print ("поїхали!")
