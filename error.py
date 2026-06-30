527fa2e206ef213cf4ab09c6f1a7e4d14cabd852
Denis Mondok <denis.mondok.2019.1@gmail.com>
2026-06-30 20:11:07 +0200

    error

---
+++ error.py
@@ -0,0 +1,7 @@
+def spance_alarm():
+    print("ТРИВОГА!")
+    print("УВАГА! ЗБІЙ У СИСТЕМІ ДМИГУНА!")
+    print("ТРИВОГА!")
+choice=input("Запустити двигун?(так?ні)")
+if choice=="ні":
+    spance_alarm()
